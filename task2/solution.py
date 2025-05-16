500867654fa9e58465edb43e00a66a615078fe6e
Станислав Рубцов <frexrub@gmail.com>
2025-05-16 14:50:00 +0300

    create set_dict_url

--- task2/solution.py
+++ task2/solution.py
@@ -2,17 +2,24 @@ import requests
 
 from bs4 import BeautifulSoup
 
-url = "https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту"
 
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
+def set_dict_url() -> dict[str, str]:
+    url = "https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту"
 
-all_data = soup.find_all("div", class_="ts-module-Индекс_категории-container")
-all_url = soup.find_all("a", class_="external text")
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
 
-dict_urls = {item.text : item["href"] for item in all_url if len(item.text) == 2}
+    all_data = soup.find_all("div", class_="ts-module-Индекс_категории-container")
+    all_url = soup.find_all("a", class_="external text")
 
-for key, val in dict_urls.items():
-    print(key, val)
+    urls = {item.text : item["href"] for item in all_url if len(item.text) == 2}
+    return urls
+
+
+if __name__ == "__main__":
+    dict_urls = set_dict_url()
+    for letter, url_letter in dict_urls.items():
+        print(letter, url_letter)
+
 
 
